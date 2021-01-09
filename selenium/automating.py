9fadd6d525a5d0de0731e6ecbfbc88834b6c7791
Pratkkumar24 <pratikkumar200024@gmail.com>
2021-01-09 10:14:42 +0530

    changed a little

--- selenium/automating.py
+++ selenium/automating.py
@@ -4,40 +4,13 @@ from selenium import webdriver
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("start-maximized")
 chrome_options.add_argument("--incognito") #using this argument the browser is opened in incognito mode
-# chrome_options.headless = True
+# chrome_options.headless = True    Using this argument you wont get the windows view
 
 driver = webdriver.Chrome(chrome_options=chrome_options)
 
-
+url = "https://transparencyreport.google.com/safe-browsing/search?hl=en"
 driver.get('https://transparencyreport.google.com/safe-browsing/search?hl=en')
-# search_box = driver.find_element_by_name('q')
-# search_box.send_keys('Youtube')
-# search_box.submit()
-# driver.refresh()
-# time.sleep(5)
-# link = driver.find_element_by_xpath('//*[@id="rso"]/div[1]/div/div/div/div[1]/a/h3/span').click()
-# # driver.refresh()
-# # time.sleep(5)
-# print("\nTitle: "+ str(driver.title))
-# print("\nURL: "+ str(driver.current_url))
-# search_box = driver.find_element_by_name('search_query')
-# print("\nTitle: "+ str(driver.title))
-# print("\nURL: "+ str(driver.current_url))
-# search_box.send_keys('Web2py Playlist')
-# search_box.submit()
-# print("\nTitle: "+ str(driver.title))
-# print("\nURL: "+ str(driver.current_url))
-# time.sleep(1)
 
-# link = driver.find_element_by_xpath('//*[@id="video-title"]').click()
-# time.sleep(1)
-# print("\nTitle: "+ str(driver.title))
-# print("\nURL: "+ str(driver.current_url))
-# link = driver.find_element_by_xpath('//*[@id="movie_player"]/div[24]/div[2]/div[1]/button').click()
-# time.sleep(1)
-
-# link = driver.find_element_by_xpath('//*[@id="movie_player"]/div[24]/div[2]/div[2]/button[9]').click()
-# time.sleep(1)
-
-# link = driver.find_element_by_xpath('//*[@id="movie_player"]/div[24]/div[2]/div[1]/button').click()
-# time.sleep(1)
+searchBox = driver.find_element_by_xpath('//*[@id="scrolling-element"]/safe-browsing-report/ng-component/section/div/search-box/input')
+searchBox.send_keys(url)
+searchBox= driver.find_element_by_xpath('//*[@id="scrolling-element"]/safe-browsing-report/ng-component/section/div/search-box/i').click()

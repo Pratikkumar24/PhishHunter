b734886375b8af588943d5ca2ee00251d3966cd5
Pratkkumar24 <pratikkumar200024@gmail.com>
2021-01-09 09:29:58 +0530

    Input method in chromeextension added

---
+++ eventPage.js
@@ -0,0 +1,15 @@
+function scanningUrl(info) {
+
+    if (info.linkUrl && !info.selectionText) {
+        chrome.tabs.create({ url: info.linkUrl })
+    } else if (info.selectionText && !info.linkUrl) {
+        chrome.tabs.create({ url: info.selectionText })
+
+    }
+}
+
+chrome.contextMenus.create({
+    title: "Scan with HunterPhisher",
+    contexts: ["link", "image", "selection"],
+    onclick: scanningUrl
+});

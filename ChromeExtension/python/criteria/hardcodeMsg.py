61642734e8c644407d2bacd9fdd677b1f8bad22e
Pratkkumar24 <pratikkumar200024@gmail.com>
2021-01-19 23:17:16 +0530

    added domainExtractor

--- ChromeExtension/python/criteria/hardcodeMsg.py
+++ ChromeExtension/python/criteria/hardcodeMsg.py
@@ -22,47 +22,22 @@ class HardCode_Scanner:
         regex = r" Share it until the blue bar is full|Share until the blue bar is full"
         return re.search(regex,(response.content).decode(errors="ignore"))
 
-class CertificateScanner(HardCode_Scanner): #use of inheritance
-    def __init__(self,CS_url):
-        super().__init__(CS_url) #inherit the methods and all from parents
-        self.sslUrl = CS_url
-
-    def addagruments(self):
-        self.chrome_options.add_argument("start-maximized")
-        self.chrome_options.add_argument("--log-level=3")
-        # self.chrome_options.headless = True
-        self.open_browser()
-
-    def open_browser(self):                                                     
-        self.driver = webdriver.Chrome(options = self.chrome_options)
-
-    def digicert(self): 
-        self.driver.get('https://www.google.com/')    
-        searchBox = self.driver.find_element_by_name("q").send_keys("Try" + Keys.RETURN)
-        return 1
-
-    def certificateScan(self):
-        self.addagruments()                                                   # adding the arguments for webdriver
-        errorCount=0
-        if not self.digicert():
-            errorCount = errorCount+1
-        return errorCount
-
-    def domainExtractor(self):                                                # domain extrater for the certificate scanning input
-        result = tldextract.extract(self.sslUrl)
-        return result.registered_domain
+    
 
 def SCAN(url):
     target_url = url
-    scan = CertificateScanner(target_url)
+    scan = HardCode_Scanner(target_url)
     
     result = scan.check_Hardcode()                      #Scanning hardcode
 
     if result == 'X':
-        return("Page not Found!!! (check your internet connection) (point 0)")
+        return("Page not Found!!!")
          
     elif result:
         return("Match Found!! Should be rejected (point 1)")
     else:
         return("Found Nothing!! (point 0)")
 
+def domainExtractor(url):                                                # domain extrater for the certificate scanning input
+    result = tldextract.extract(url)
+    return result.registered_domain
